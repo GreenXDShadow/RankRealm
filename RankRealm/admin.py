80b444589b7f8f787f9bffc45ebb93119db51568
GreenXDShadow <NickDValen@gmail.com>
2024-03-03 23:54:47 -0800

    DB set up DONE. Time to work on front end

--- RankRealm/admin.py
+++ RankRealm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile, Game, Leaderboard, PlayerPerformance, Event
 
+admin.site.register(UserProfile)
+admin.site.register(Game)
+admin.site.register(Leaderboard)
+admin.site.register(PlayerPerformance)
+admin.site.register(Event)
 # Register your models here.
