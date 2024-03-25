78c74338b51547685da47aa5ae3ca2920b63a2cf
GreenXDShadow <NickDValen@gmail.com>
2024-03-25 00:15:48 -0700

    moved some stuff around

--- RankRealm/views.py
+++ RankRealm/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import * # This imports all our models into here
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index page.")
-
 
 def gameinfo(request, game_id):
     return HttpResponse("You're looking at %s." % game_id)
